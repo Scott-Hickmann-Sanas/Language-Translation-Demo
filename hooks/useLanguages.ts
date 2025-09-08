130aebe89815797a01d9d4d534107858a58772c7
Scott Hickmann <scott.hickmann@sanas.ai>
2025-09-08 12:17:10 -0700

    Pass API key to languages

--- hooks/useLanguages.ts
+++ hooks/useLanguages.ts
@@ -6,10 +6,20 @@ if (!process.env.NEXT_PUBLIC_LT_ENDPOINT) {
 }
 const LT_ENDPOINT = process.env.NEXT_PUBLIC_LT_ENDPOINT;
 
+if (!process.env.NEXT_PUBLIC_SANAS_API_KEY) {
+  throw new Error("NEXT_PUBLIC_SANAS_API_KEY is not set");
+}
+const SANAS_API_KEY = process.env.NEXT_PUBLIC_SANAS_API_KEY;
+
 export function useLanguages() {
   const [languages, setLanguages] = useState<Languages>([]);
   useEffect(() => {
-    fetch(`${LT_ENDPOINT}/languages`)
+    fetch(`${LT_ENDPOINT}/languages`, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": SANAS_API_KEY,
+      },
+    })
       .then((res) => res.json())
       .then((data) => setLanguages(Languages.parse(data)));
   }, []);
