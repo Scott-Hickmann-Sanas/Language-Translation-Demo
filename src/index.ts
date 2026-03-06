790040a0ea8780e66c18f00a202c711e363b8ab3
Scott Hickmann <scott.hickmann@sanas.ai>
2026-03-06 15:11:59 -0800

    Export all types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
 export { SanasTranslationClient } from "./SanasTranslationClient";
-export type {
-  ConnectionState,
-  ConnectOptions,
-  ConnectResult,
-  FetchLanguagesOptions,
-  IdentifiedLanguageDisplay,
-  Language,
-  ResetOptions,
-  SanasTranslationClientOptions,
-  TranslationClientState,
-  UtteranceDisplay,
-  UtteranceStreamDisplay,
-} from "./types";
+export type * from "./types";
